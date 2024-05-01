62467380a1e4c60408364875f685ca05f57e43e8
bigtree_it_s <bigtree.it.services@gmail.com>
2024-05-01 13:10:11 +0100

    Add  Foods, Shared Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SharedModule } from './shared';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
+    SharedModule,
     AppRoutingModule,
     NgbModule
   ],
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './pages/header/header.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports:[
+    HeaderComponent
+  ]
+})
+export class SharedModule { }
